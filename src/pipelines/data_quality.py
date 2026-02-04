14c32266debac6b53fac72f8d794732bfda93e17
Nathã Melo <nathamelo2007@gmail.com>
2026-02-03 22:47:02 -0300

    fix: remove comentarios

--- src/pipelines/data_quality.py
+++ src/pipelines/data_quality.py
@@ -19,30 +19,3 @@ def insight_dirty_cost():
     df_order_dirty_fat['data'] = pd.to_datetime(df_order_dirty_fat['data'], format='mixed', errors='coerce').dt.year
 
     return df_order_dirty_fat, df_order_clean
-
-
-
-#     # concatenando e fazendo agreagações
-#     df_concat = pd.concat([df_order_dirty_fat, df_order_clean], axis=1)
-#     df_concat_stats = df_concat.agg({
-#     'id_venda': ['count'],
-#     'id_order': ['count'],
-#     'valor': ['sum'],
-#     'price': ['sum']
-# })
-
-#     # soma para tirar os nulls
-#     df_final = df_concat_stats.sum().to_frame().T
-
-#     df_final.columns = ['orders_dirty', 'orders_clean', 'sum_dirty', 'sum_clean']
-#     df_final['diff_counts'] = df_final['orders_dirty'] - df_final['orders_clean']
-    
-#     # stack para transformar as colunas em linhas
-#     df_stack = df_final.stack().reset_index()
-#     df_stack.columns = ['index', 'Tipo', 'Valores']
-#     df_stack.drop('index', axis=1, inplace=True)
-
-#     return df_stack
-
-if __name__ == '__main__':
-    df = insight_dirty_cost()
